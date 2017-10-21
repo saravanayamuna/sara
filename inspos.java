dcd8514f6a8362c959acc09cb20546d5a8d34b4c
saravanayamuna <saravan.kcse@gmail.com>
2017-10-21 10:56:21 +0530

    Add files via upload

---
+++ inspos.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class bsb {
+	public static void main(String [] args)
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int N=input.nextInt();
+		int [] arr=new int[N+1];
+		System.out.println("Enter array elemnents");
+		for(int i=0;i<N;i++)
+		{
+			arr[i]=input.nextInt();
+		}
+		System.out.println("Enter the number to be inserted ");
+		int num=input.nextInt();
+		System.out.println("Enter the postion");
+		int pos=input.nextInt();
+		for(int i=N-1;i>=pos-1;i--)
+		{
+			
+			arr[i+1]=arr[i];
+		
+			
+		}
+		arr[pos-1]=num;
+		for(int i=0;i<N;i++)
+		{
+			System.out.print(" "+arr[i]);
+		}
+	}
+
+}
